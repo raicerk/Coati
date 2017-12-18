95db84d7d62004370fa3bcb9bf54fbea76db92de
raicerk <raicerk@gmail.com>
2017-12-17 23:04:31 -0300

    Se agregan nuevas funcionalidades

--- main.js
+++ main.js
@@ -1,92 +1,165 @@
 const botgram = require("botgram")
-const bot = botgram("Token")
+const bot = botgram("InserteTokenAqui")
 const fetch = require("node-fetch");
 var estadoAnalisis = true;
 
 bot.command("Start", (msg, reply) => {
-    try{
-        reply.text("Bienvenido a Coati un bot que te avisa cuando el valor de la Chaucha sube o baja :D usa el comando /avisa (Mayor/Menor) Valor cada X (Segundos) como por ejemplo: ")
-        reply.text("/Avisa Mayor 500 cada 30 segundos")
-        reply.text("/Avisa Menor 500 cada 40 segundos")
-        reply.text("Si quieres detener el bot para que no lleguen mas notificaciones solo escribe /Parar ")
-    }catch(Exception){
-        console.log(Exception.message);
-    }
+  try {
+    reply.text("Bienvenido a Coati un bot que te avisa cuando el valor de la Chaucha sube o baja de x monto con sistema de revision por segundos por ejemplo: ")
+    reply.text("/Mayor 500 30")
+    reply.text("/Menor 500 40")
+    reply.text("/Avisa 400 500 20")
+    reply.text("Si quieres detener el bot para que no lleguen mas notificaciones solo escribe /Parar ")
+  } catch (Exception) {
+    console.log(Exception.message);
+  }
 })
 
 bot.command("Avisa", (msg, reply) => {
 
-    try{
+  try {
 
-        if(msg){
-            estadoAnalisis = true;
-        }
-
-        var datos = msg.args(1)[0].toString().split(" ");
-
-        var Estado = datos[0];
-        var Valor = parseInt(datos[1]);
-        var TiempoSegundos = parseInt(datos[3]);
-        var ValorCriptoMoneda = 0;
-
-        console.log(`El valor ingresado es: ${Valor}`);
-        console.log(`El estado ingresado es: ${Estado}`);
-        console.log(`El tiempo de actualizacion ingresado es: ${TiempoSegundos}`);
-
-        setInterval(function () {
-
-            if (estadoAnalisis) {
-
-                const url = "https://api.orionx.io/graphql?query={marketOrderBook(marketCode:%22CHACLP%22){mid}}";
-
-                fetch(url)
-                    .then(response => {
-
-                        response.json().then(json => {
-
-                            ValorCriptoMoneda = parseInt(json.data.marketOrderBook.mid);
-
-                            switch (Estado) {
-                                case "Mayor":
-                                    if (ValorCriptoMoneda > Valor) {
-                                        reply.text(`Vende Chauchas, su valor actual es de ${ValorCriptoMoneda}`)
-                                    }
-                                    break;
-
-                                case "Menor":
-                                    if (ValorCriptoMoneda < Valor) {
-                                        reply.text(`Compra Chauchas, su valor actual es de ${ValorCriptoMoneda}`)
-                                    }
-                                    break;
-
-                                default:
-                                    break;
-                            }
-                        });
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            }
-
-        }, (TiempoSegundos * 1000));
-
-    }catch(Exception){
-        console.log(Exception.message)
+    if (msg) {
+      estadoAnalisis = true;
     }
+
+    var datos = msg.args(1)[0].toString().split(" ");
+
+    var Valor1 = parseInt(datos[0]);
+    var Valor2 = parseInt(datos[1]);
+    var TiempoSegundos = parseInt(datos[2]);
+    var ValorCriptoMoneda = 0;
+
+    console.log(`El valor 1 ingresado es: $${Valor1}`);
+    console.log(`El valor 2 ingresado es: $${Valor2}`);
+    console.log(`El tiempo de actualizacion ingresado es: ${TiempoSegundos} segundos`);
+
+    setInterval(function() {
+
+      if (estadoAnalisis) {
+
+        const url = "https://api.orionx.io/graphql?query={marketOrderBook(marketCode:%22CHACLP%22){mid}}";
+
+        fetch(url)
+          .then(response => {
+
+            response.json().then(json => {
+
+              ValorCriptoMoneda = parseInt(json.data.marketOrderBook.mid);
+
+              if (ValorCriptoMoneda > Valor1 && ValorCriptoMoneda < Valor2) {
+                reply.text(`La chaucha esta a $${ValorCriptoMoneda}`)
+              }
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+
+    }, (TiempoSegundos * 1000));
+    reply.text(`Te avisare cuando la chaucha este entre $${Valor1} y $${Valor2}` )
+  } catch (Exception) {
+    console.log(Exception.message)
+  }
 })
 
 bot.command("Parar", (msg, reply) => {
 
-    try{
-        estadoAnalisis = false;
-        reply.text("Se ha detenido en analisis de las Chauchas")
-    }catch(Exception){
-        console.log(Exception.message);
+  try {
+    estadoAnalisis = false;
+    reply.text("Se ha detenido en analisis de las Chauchas")
+  } catch (Exception) {
+    console.log(Exception.message);
+  }
+
+})
+
+bot.command("Mayor", (msg, reply) => {
+
+  try {
+
+    if (msg) {
+      estadoAnalisis = true;
+    }
+
+    var datos = msg.args(1)[0].toString().split(" ");
+
+    var Valor = parseInt(datos[0]);
+    var TiempoSegundos = parseInt(datos[1]);
+    var ValorCriptoMoneda = 0;
+
+    console.log(`El valor ingresado es: $${Valor}`);
+    console.log(`El tiempo de actualizacion ingresado es: ${TiempoSegundos} segundos`);
+
+    setInterval(function() {
+
+      if (estadoAnalisis) {
+
+        const url = "https://api.orionx.io/graphql?query={marketOrderBook(marketCode:%22CHACLP%22){mid}}";
+
+        fetch(url).then(response => {
+            response.json().then(json => {
+              ValorCriptoMoneda = parseInt(json.data.marketOrderBook.mid);
+              if (ValorCriptoMoneda > Valor) {
+                reply.text(`Vende Chauchas, su valor actual es de $${ValorCriptoMoneda}`)
+              }
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    }, (TiempoSegundos * 1000));
+    reply.text(`Te avisare cuando el valor de la chaucha sea mayor a $${Valor}`);
+  } catch (Exception) {
+    console.log(Exception.message)
+  }
+})
+
+bot.command("Menor", (msg, reply) => {
+
+    try {
+
+      if (msg) {
+        estadoAnalisis = true;
+      }
+
+      var datos = msg.args(1)[0].toString().split(" ");
+
+      var Valor = parseInt(datos[0]);
+      var TiempoSegundos = parseInt(datos[1]);
+      var ValorCriptoMoneda = 0;
+
+      console.log(`El valor ingresado es: $${Valor}`);
+      console.log(`El tiempo de actualizacion ingresado es: ${TiempoSegundos} segundos`);
+
+      setInterval(function() {
+
+        if (estadoAnalisis) {
+
+          const url = "https://api.orionx.io/graphql?query={marketOrderBook(marketCode:%22CHACLP%22){mid}}";
+
+          fetch(url).then(response => {
+              response.json().then(json => {
+                ValorCriptoMoneda = parseInt(json.data.marketOrderBook.mid);
+                if (ValorCriptoMoneda < Valor) {
+                  reply.text(`Compra Chauchas, su valor actual es de $${ValorCriptoMoneda}`)
+                }
+              });
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      }, (TiempoSegundos * 1000));
+      reply.text(`Te avisare cuando el valor de la chaucha sea menor a $${Valor}`);
+    } catch (Exception) {
+      console.log(Exception.message)
     }
 
 })
 
 bot.command((msg, reply) => {
-    reply.text("Invalid command.")
+  reply.text("Invalid command.")
 })
